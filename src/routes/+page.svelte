b748df66592bdd0ebf1a860262133887bd866f25
Noam Shemesh <noams@hey.com>
2024-02-25 13:18:38 +0200

    ⚡️improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,10 @@
 
 <div class="grid flex-1 content-center justify-center text-center">
   <h1 class="my-2 text-3xl font-bold md:text-6xl">WELCOME</h1>
-  <div class="bg-primary text-primary-foreground w-full rounded-md p-2 text-center text-sm font-semibold md:text-xl">
-    <p>{data.user ? data.user.email : ''}</p>
-  </div>
+
+  {#if data.user}
+    <div class="bg-primary text-primary-foreground w-full rounded-md p-2 text-center text-sm font-semibold md:text-xl">
+      <p>{data.user ? data.user.email : ''}</p>
+    </div>
+  {/if}
 </div>
